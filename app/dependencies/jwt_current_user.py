f76b326b84dab4b622f7dd3fac86cdb32a74e98c
Emmanuel Obolo <e.obolo@alustudent.com>
2025-11-11 17:23:42 +0200

    new updates

--- app/dependencies/jwt_current_user.py
+++ app/dependencies/jwt_current_user.py
@@ -1,10 +1,8 @@
-from fastapi import Depends, HTTPException, status, Request
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from app.utils.jwt_utils import verify_token
-from fastapi.templating import Jinja2Templates
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
-templates = Jinja2Templates(directory="templates")
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     payload = verify_token(token)
